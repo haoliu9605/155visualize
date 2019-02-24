b0cd2aee9e5dbe05f5a6af1e69a2a7dfc42fea4a
haoliu9605 <haoliu9605@gmail.com>
2019-02-24 12:09:46 -0800

    grid search

--- 5c.py
+++ 5c.py
@@ -23,19 +23,20 @@ def main():
     Y_test = Dataset.load_from_file('./data/test.txt', reader=reader)
     ytest = Y_test.build_full_trainset()
     testset = ytest.build_testset()
-    '''        
+         
     # grid search
-    param_grid = {'n_epochs': [5, 10], 'lr_all': [0.002, 0.005],
-              'reg_all': [0.4, 0.6]}
+    param_grid = {'n_factors':[20], 'n_epochs': [20], 'lr_all': [0.01,0.03, 0.05],
+              'reg_all': [0.1, 0.2,0.3]}
     
-    gs = GridSearchCV(SVD, param_grid, measures=['rmse'], cv=3)
+    gs = GridSearchCV(SVD, param_grid, measures=['rmse'], cv=5)
     gs.fit(Y_train)
     # best RMSE score
     print(0.5*gs.best_score['rmse']**2)
     # combination of parameters that gave the best RMSE score
     print(gs.best_params['rmse'])
-    '''
+
         
+    '''
     algo = SVD(n_factors=20, n_epochs=20, lr_all=0.03, reg_all=0.1,verbose=True)        
     algo.fit(trainset)    
     
@@ -45,7 +46,7 @@ def main():
     U = np.transpose(algo.pu)    # ndarray of size (user x factor)
     V = np.transpose(algo.qi)    # ndarray of size (item x factor)
     
-    
+    '''
     
 if __name__ == "__main__":
     main()
