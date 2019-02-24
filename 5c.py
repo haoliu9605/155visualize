a8988701e4c138be787a864a265395d2ca18966f
haoliu9605 <haoliu9605@gmail.com>
2019-02-24 11:49:33 -0800

    add grid serach and get U,V

--- 5c.py
+++ 5c.py
@@ -4,35 +4,48 @@ import matplotlib.pyplot as plt
 from surprise import BaselineOnly
 from surprise import Dataset
 from surprise import Reader
-from surprise.model_selection import cross_validate
-from surprise.model_selection import train_test_split
+from surprise.model_selection import GridSearchCV
 
-from surprise import SVD
+
+from surprise import SVD, SVDpp
 from surprise import accuracy
 
 
-#trainset, testset = train_test_split(Y1_train, test_size=.25)    
-#Y_train = np.loadtxt('data/train.txt').astype(int)
-#Y_test = np.loadtxt('data/test.txt').astype(int)
+
 
 def main():
     
     reader = Reader(line_format='user item rating', sep='\t',rating_scale=(1, 5))
-    Y1_train = Dataset.load_from_file('data/train.txt', reader=reader)
-    trainset = Y1_train.build_full_trainset()
-    Y1_test = Dataset.load_from_file('./data/test.txt', reader=reader)
-    Y_test = Y1_test.build_full_trainset()
-    testset = Y_test.build_testset()
+    
+    Y_train = Dataset.load_from_file('data/train.txt', reader=reader)
+    trainset = Y_train.build_full_trainset()
+    
+    Y_test = Dataset.load_from_file('./data/test.txt', reader=reader)
+    ytest = Y_test.build_full_trainset()
+    testset = ytest.build_testset()
+    '''        
+    # grid search
+    param_grid = {'n_epochs': [5, 10], 'lr_all': [0.002, 0.005],
+              'reg_all': [0.4, 0.6]}
+    
+    gs = GridSearchCV(SVD, param_grid, measures=['rmse'], cv=3)
+    gs.fit(Y_train)
+    # best RMSE score
+    print(0.5*gs.best_score['rmse']**2)
+    # combination of parameters that gave the best RMSE score
+    print(gs.best_params['rmse'])
+    '''
         
-    algo = SVD(n_factors=50, n_epochs=100, lr_all=0.15, reg_all=0.1,verbose=True)
-        
+    algo = SVD(n_factors=20, n_epochs=20, lr_all=0.03, reg_all=0.1,verbose=True)        
     algo.fit(trainset)    
     
-    predictions = algo.test(testset)
-    accuracy.rmse(predictions)
+    predictions = algo.test(testset)    
+    print(0.5*accuracy.rmse(predictions)**2) # rescale as the hw error
     
-    #predictions1 = algo.test(trainset)
-    #accuracy.rmse(predictions1)
-
+    U = np.transpose(algo.pu)    # ndarray of size (user x factor)
+    V = np.transpose(algo.qi)    # ndarray of size (item x factor)
+    
+    
+    
 if __name__ == "__main__":
     main()
