2ac9aebb8f927e6c193a6a74f2eb50625e195c20
haoliu9605 <haoliu9605@gmail.com>
2019-02-27 22:43:45 -0800

    change 5C

--- recsys_5C.py
+++ recsys_5C.py
@@ -13,7 +13,7 @@ from surprise import accuracy
 
 
 
-def main():
+def getUV():
 
     reader = Reader(line_format='user item rating', sep='\t',rating_scale=(1, 5))
 
@@ -24,6 +24,11 @@ def main():
     ytest = Y_test.build_full_trainset()
     testset = ytest.build_testset()
 
+    Y_data = Dataset.load_from_file('./data/data.txt', reader=reader)
+    train_data = Y_data.build_full_trainset()
+
+    test_data = train_data.build_testset()
+    '''
     # grid search
     param_grid = {'n_factors':[20], 'n_epochs': [20], 'lr_all': [0.01,0.03, 0.05],
               'reg_all': [0.1, 0.2,0.3]}
@@ -34,19 +39,24 @@ def main():
     print(0.5*gs.best_score['rmse']**2)
     # combination of parameters that gave the best RMSE score
     print(gs.best_params['rmse'])
-
-
     '''
+
+    # see performance on testset
+    algo1 = SVD(n_factors=20, n_epochs=20, lr_all=0.03, reg_all=0.1,verbose=True)
+    algo1.fit(trainset)
+
+    predictions1 = algo1.test(testset)
+    print("test ",0.5*accuracy.rmse(predictions1)**2) # rescale as the hw error
+
+    # see visualization
     algo = SVD(n_factors=20, n_epochs=20, lr_all=0.03, reg_all=0.1,verbose=True)
-    algo.fit(trainset)
+    algo.fit(train_data)
 
-    predictions = algo.test(testset)
-    print(0.5*accuracy.rmse(predictions)**2) # rescale as the hw error
+    predictions = algo.test(test_data)
+    print("data ",0.5*accuracy.rmse(predictions)**2) # rescale as the hw error
 
-    U = np.transpose(algo.pu)    # ndarray of size (user x factor)
-    V = np.transpose(algo.qi)    # ndarray of size (item x factor)
+    U = algo.pu    # ndarray of size (user x factor)
+    V = algo.qi    # ndarray of size (item x factor)
 
-    '''
 
-if __name__ == "__main__":
-    main()
+    return U,V
