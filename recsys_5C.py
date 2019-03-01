cb8c9fd3930f2cea0a15c32ed742bc961ea4aeb6
haoliu9605 <haoliu9605@gmail.com>
2019-02-28 16:19:06 -0800

    fix index issue of Surprise

--- recsys_5C.py
+++ recsys_5C.py
@@ -10,7 +10,7 @@ from surprise.model_selection import GridSearchCV
 from surprise import SVD, SVDpp
 from surprise import accuracy
 
-
+# to_inner_uid(), to_inner_iid(), to_raw_uid(), and to_raw_iid()
 
 
 def getUV():
@@ -19,6 +19,7 @@ def getUV():
 
     Y_train = Dataset.load_from_file('data/train.txt', reader=reader)
     trainset = Y_train.build_full_trainset()
+    train_perf = trainset.build_testset()
 
     Y_test = Dataset.load_from_file('./data/test.txt', reader=reader)
     ytest = Y_test.build_full_trainset()
@@ -45,11 +46,14 @@ def getUV():
     algo1 = SVD(n_factors=20, n_epochs=20, lr_all=0.03, reg_all=0.1,verbose=True)
     algo1.fit(trainset)
 
+    predictions2 = algo1.test(train_perf)
+    print("train ",0.5*accuracy.rmse(predictions2)**2) # rescale as the hw error
+
     predictions1 = algo1.test(testset)
     print("test ",0.5*accuracy.rmse(predictions1)**2) # rescale as the hw error
 
     # see visualization
-    algo = SVD(n_factors=20, n_epochs=20, lr_all=0.03, reg_all=0.1,verbose=True)
+    algo = SVD(n_factors=20, n_epochs=35, lr_all=0.03, reg_all=0.1,verbose=True)
     algo.fit(train_data)
 
     predictions = algo.test(test_data)
@@ -57,6 +61,17 @@ def getUV():
 
     U = algo.pu    # ndarray of size (user x factor)
     V = algo.qi    # ndarray of size (item x factor)
-
-
+    
+    
+    # remapping of U, V:
+    #U_ind = [] # U_ind[i] = j, then now ith row of U should be map to jth row of the new mat
+    #for i in range(len(U)):
+    #    U_ind.append(train_data.to_raw_iid(i))
+    #V_ind = []    
+    #for i in range(len(V)):
+    #   V_ind.append(train_data.to_raw_uid(i))
+    U_vec = range(len(U))
+    U[[int(train_data.to_raw_uid(i))-1 for i in U_vec]] = U[U_vec]   
+    V_vec = range(len(V))
+    V[[int(train_data.to_raw_iid(i))-1 for i in V_vec]] = V[V_vec]
     return U,V
